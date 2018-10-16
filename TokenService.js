115add4cf08c74908f86ec65a17e567bfe177682
Jesse Alves <jessecarlos@gmail.com>
2018-10-16 16:36:07 -0300

    Fixes on getToken

--- TokenService.js
+++ TokenService.js
@@ -18,7 +18,7 @@ const TokenService = {
                 if (tokenHasExpired) {
                     await IdbKeycloak.get(TOKEN_VALUE)
                         .then(({ refresh_token }) => {
-                            KeycloakService.refreshToken(keycloakConfig, refresh_token)
+                            await KeycloakService.refreshToken(keycloakConfig, refresh_token)
                             .then(this.setToken);
                         })
                 }
